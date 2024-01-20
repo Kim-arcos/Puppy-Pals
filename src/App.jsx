bdc55f30e51c7a65ee5fbe704eada5417bbe64c0
Kim-arcos <kimarcos16@gmail.com>
2024-01-19 22:04:09 -0700

    workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { puppyList } from './data.js';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies);
+
   return (
     <div>
-      <a href="https://vitejs.dev" target="_blank">
+      <a href="https://vitejs.svg" target="_blank">
         <img src={viteLogo} className="logo" alt="Vite logo" />
       </a>
-      <a href="https://react.dev" target="_blank">
+      <a href="https://react.svg" target="_blank">
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
     </div>
