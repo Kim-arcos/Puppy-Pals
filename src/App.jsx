7e03acb20c05b46f81881bbccc9e3b0c19c9328e
Kim-arcos <kimarcos16@gmail.com>
2024-01-22 18:20:56 -0700

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
-import { puppyList } from "./data.js";
+import React, { useState } from 'react';
+import { puppyList } from './data.js';
+import './App.css'
+
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log(puppies);
+  console.log("puppyList: ", puppyList);
+
+  function handleClick(puppyId) {
+    console.log("puppy id: ", puppyId);
+    setFeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("featuredPup: ", featuredPup);
 
   return (
-    <div>
-      {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
-      })}
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
