90b781e7a024dc72a82ec82f0e18d6a4443c401f
Kim-arcos <kimarcos16@gmail.com>
2024-01-22 17:25:51 -0700

    second tab completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import { puppyList } from './data.js';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
@@ -10,12 +8,9 @@ function App() {
 
   return (
     <div>
-      <a href="https://vitejs.svg" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.svg" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
     </div>
   );
 }
